6b72507d3c6d5686a97a92c5af31a971b80c8ffa
yedijas <bananab9001@gmail.com>
2021-09-06 15:00:46 +0700

    Add project files.

---
+++ SimpleExcelToDataTable/SimpleDataTable.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace SimpleExcelToDataTable
+{
+    partial class SimpleDataTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.myOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileLocation = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtDataResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(713, 38);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // myOpenFileDialog
+            // 
+            this.myOpenFileDialog.FileName = "myOpenFileDialog";
+            // 
+            // txtFileLocation
+            // 
+            this.txtFileLocation.Location = new System.Drawing.Point(12, 12);
+            this.txtFileLocation.Name = "txtFileLocation";
+            this.txtFileLocation.Size = new System.Drawing.Size(776, 20);
+            this.txtFileLocation.TabIndex = 1;
+            this.txtFileLocation.Text = "C:\\temp\\testData.xlsx";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(632, 38);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtDataResult
+            // 
+            this.txtDataResult.Location = new System.Drawing.Point(12, 67);
+            this.txtDataResult.Multiline = true;
+            this.txtDataResult.Name = "txtDataResult";
+            this.txtDataResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDataResult.Size = new System.Drawing.Size(776, 371);
+            this.txtDataResult.TabIndex = 3;
+            // 
+            // SimpleDataTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDataResult);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileLocation);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "SimpleDataTable";
+            this.Text = "Test Data Table";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.OpenFileDialog myOpenFileDialog;
+        private System.Windows.Forms.TextBox txtFileLocation;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtDataResult;
+    }
+}
+
---
+++ SimpleExcelToDataTable/SimpleDataTable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using Newtonsoft.Json;
+
+namespace SimpleExcelToDataTable
+{
+    public partial class SimpleDataTable : Form
+    {
+        public SimpleDataTable()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == myOpenFileDialog.ShowDialog())
+            {
+                txtFileLocation.Text = myOpenFileDialog.FileName;
+            }
+            else
+            {
+                txtFileLocation.Text = "";
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtFileLocation.Text) && File.Exists(txtFileLocation.Text))
+            {
+                var dt = ReadExcel(txtFileLocation.Text);
+                txtDataResult.Text = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            }
+            else
+            {
+                MessageBox.Show("Invalid File!");
+            }
+        }
+
+        private DataTable ReadExcel(string path)
+        {
+            IWorkbook workbook;
+            DataTable myDt = new DataTable("table");
+
+            // try to load workbook as xlsx if failed then xls.
+            try
+            {
+                using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    try
+                    {
+                        workbook = new XSSFWorkbook(stream);
+                    }
+                    catch
+                    {
+                        workbook = null;
+                    }
+                    if (workbook == null)
+                    {
+                        workbook = new HSSFWorkbook(stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Excel read error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return myDt; 
+            }
+
+            var sheet = workbook.GetSheetAt(0);
+
+            var headerRow = sheet.GetRow(0);
+            foreach(var headerCell in headerRow)
+            {
+                myDt.Columns.Add(headerCell.ToString());
+            }
+
+            for(int i = 1; i < sheet.PhysicalNumberOfRows; i++)
+            {
+                var sheetRow = sheet.GetRow(i);
+                var dtRow = myDt.NewRow();
+                dtRow.ItemArray = myDt.Columns
+                    .Cast<DataColumn>()
+                    .Select(o => sheetRow.GetCell(o.Ordinal, MissingCellPolicy.CREATE_NULL_AS_BLANK).ToString())
+                    .ToArray();
+                myDt.Rows.Add(dtRow);
+            }
+
+            return myDt;
+        }
+    }
+}
